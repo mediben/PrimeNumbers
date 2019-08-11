ae70bf1fbd6798685450ce4fbb11221931f382ab
Mehdi Ben taarit <mehdibentaarit@Mehdis-MacBook-Pro.local>
2019-08-12 00:27:50 +0200

    Add pagination to result

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_paginate import Pagination, get_page_args
+
 from .views import get_primes
 
 application = Flask(__name__)
@@ -6,10 +8,23 @@ application = Flask(__name__)
 @application.route('/primes',  methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
         data = request.get_json(force=True) 
-        res = get_primes(data)
-        return jsonify(msg=res)
-    
+        res = get_primes(int(data['number'])+1)
+        total = len(res)
+        pagination_primes = paginate_result(res, offset=offset, per_page=per_page)
+        pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+        
+        return render_template('index.html',
+                           numbers=pagination_primes,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
     elif request.method == 'GET':
         pass
         
+def paginate_result(primes, offset=0, per_page=100):
+    return primes[offset: offset + per_page]
